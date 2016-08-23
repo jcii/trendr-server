da5a1b6ce2afe6d79a4736b6f9838090537d48fa
Alexander Murphy <amurphy345@gmail.com>
2016-08-23 13:27:29 -0600

    adding passport controller

---
+++ app/Controllers/authControllers/passportStrategyController.js
@@ -0,0 +1,53 @@
+// 'use strict'
+// const passport = require('passport')
+// const bcrypt = require('bcrypt')
+// const LocalStrategy = require('passport-local').Strategy;
+// const BearerStrategy = require('passport-http-bearer').Strategy;
+// const userModelClass = require('../../Models/authModels/userModel')
+// const userModel = new userModelClass
+
+// module.exports = (passport) => {
+//   passport.use(
+//   new LocalStrategy({
+//       usernameField: 'username',
+//       passwordField: 'password',
+//       session: true },
+//     (username, password, done) => {
+//         process.nextTick(() => {
+//           userModel.checkUserLogin({username})
+//             .catch((e) => done(null, false))
+//             .then((collection) => {
+//             if(collection.length > 0){
+//               if(bcrypt.compareSync(password, collection[0].password)){
+//                 return done(null, collection[0])
+//               } else {
+//                 return done(null, false)
+//               }
+//             } else {
+//               return done(null, false)
+//             }
+//         })
+//       })
+//     }
+//   )
+// )
+// passport.use(new BearerStrategy(
+//   (token, done) => {
+//     userModel.checkUserToken(token).then((user) => {
+//       if(user.length == 0){
+//         return done(null, false)
+//       } else {
+//         return done(null, user, { scope: 'all' });
+//       }
+//     })
+//   }
+// ))
+
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
+
+// passport.deserializeUser(function(user, done) {
+//   done(null, user);
+// });
+// }
---
+++ app/Models/authModels/userModel.js
@@ -0,0 +1,31 @@
+// 'use strict'
+
+// const db = require('../../config/db/knex/knexConfig')
+// const bcrypt = require('bcrypt')
+// const randToken = require('rand-token')
+// module.exports = class userModel {
+//   constructor() {}
+//   getUser() {
+//     return db.knex('feedz_users')
+//   }
+//   createUserIfNotExists(user) {
+//     console.log(user)
+//      return db.knex('feedz_users').where('username', user.username)
+//       .then((userResponse) => {
+//         if(userResponse.length == 0){
+//           return db.knex('feedz_users').insert({
+//             username: user.username, 
+//             password: bcrypt.hashSync(user.password, 10), 
+//             email:user.email,
+//             token: randToken.generate(16)
+//           })
+//         }
+//       })
+//   }
+//   checkUserLogin(user) {
+//     return db.knex('feedz_users').where(user)
+//   }
+//   checkUserToken(token) {
+//     return db.knex('feedz_users').where({token})
+//   }
+// }
