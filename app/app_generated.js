c62bf45a1f979889ba9715010f5d49d2567b4deb
Alexander Murphy <amurphy345@gmail.com>
2016-08-25 19:31:28 -0600

    adding typescript to server

---
+++ app/app_generated.js
@@ -0,0 +1,14 @@
+"use strict";
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes);
+module.exports = app;
--- app/routes/routeExporter.js
+++ app/routes/routeExporter.js
@@ -1,3 +1,3 @@
 module.exports = {
-  '/': require('./API/rootRoute')
-}
+    '/': require('./API/rootRoute')
+};
