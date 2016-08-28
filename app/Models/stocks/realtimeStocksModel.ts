a5dc8c38d10ba5cbddcd86bf0d374ffe87c1510c
cole sobel <colesobel@coles-MacBook-Air.local>
2016-08-28 17:33:20 -0600

    indent fix

--- app/Models/stocks/realtimeStocksModel.ts
+++ app/Models/stocks/realtimeStocksModel.ts
@@ -3,15 +3,15 @@ const db = require('../../config/db/knex/knexConfig')
 const request = require('request')
 
 module.exports = class realtimeStocks {
-  constructor() { }
-  getRealtimeStockPrice() {
+    constructor() { }
+    getRealtimeStockPrice() {
     return new Promise((resolve: any, reject: any) => {
         request(`http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=NFLX`, (error, response, body) => {
             return (!error && response.statusCode == 200) ? resolve(JSON.stringify(body)) : reject(error)
         })
 
     })
-  }
+    }
 
     updateDatabase(obj: any) {
         let unixDate: string = `${obj.Timestamp.substring(8,10)}-${obj.Timestamp.substring(4,7)}-${obj.Timestamp.substring(30, 35)} ${obj.Timestamp.substring(11,19)}`
