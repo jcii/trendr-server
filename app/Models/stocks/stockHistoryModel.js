a3c909c4f8f5416e6170175e74d804f6d1a00035
cole sobel <colesobel@coles-MacBook-Air.local>
2016-08-29 14:58:29 -0600

    stock history table correclty dropping old records when new ones added, returning data to client

--- app/Models/stocks/stockHistoryModel.js
+++ app/Models/stocks/stockHistoryModel.js
@@ -1,14 +1,32 @@
 'use strict';
-var db = require('../../config/db/knex/knexConfig');
-var request = require('request');
+var stockHistorydb = require('../../config/db/knex/knexConfig');
+var stockHistoryRequest = require('request');
 module.exports = (function () {
     function StockHistoryClass() {
     }
     StockHistoryClass.prototype.getStockHistory = function (obj) {
         var url = "http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={\"Normalized\":false,\"NumberOfDays\":" + obj.NumberOfDays + ",\"DataPeriod\":\"" + obj.DataPeriod + "\",\"Elements\":[{\"Symbol\":\"" + obj.Symbol + "\",\"Type\":\"price\",\"Params\":[\"c\"]}]}";
         return new Promise(function (resolve, reject) {
-            request(url, function (error, response, body) {
-                return (!error && response.statusCode == 200) ? resolve(body) : reject(error);
+            stockHistoryRequest(url, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    stockHistorydb.knex.raw("delete from stock_history").then(function () {
+                        var data = JSON.parse(body);
+                        var datesArray = data.Dates.map(function (date) { return new Date(date.substring(0, 10)).getTime() / 1000; });
+                        var pricesArray = data.Elements[0].DataSeries.close.values;
+                        var databaseArray = [];
+                        for (var i = 0; i < datesArray.length; i++) {
+                            databaseArray.push(stockHistorydb.knex.raw("insert into stock_history values (default, 1, 'netflix', '" + data.Elements[0].Symbol + "', " + pricesArray[i] + ", '" + obj.DataPeriod + "', " + datesArray[i] + ")").then(function (data) {
+                                return data;
+                            }));
+                        }
+                        Promise.all(databaseArray).then(function (promiseData) {
+                            return stockHistorydb.knex.raw("select * from stock_history").then(function (finalData) { return resolve(finalData.rows); });
+                        });
+                    });
+                }
+                else {
+                    reject(error);
+                }
             });
         });
     };
--- app/Models/stocks/stockHistoryModel.ts
+++ app/Models/stocks/stockHistoryModel.ts
@@ -1,6 +1,6 @@
 'use strict'
-const db = require('../../config/db/knex/knexConfig')
-const request = require('request')
+const stockHistorydb = require('../../config/db/knex/knexConfig')
+const stockHistoryRequest = require('request')
 
 
 module.exports = class StockHistoryClass {
@@ -8,8 +8,27 @@ module.exports = class StockHistoryClass {
     getStockHistory(obj) {
         let url = `http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={"Normalized":false,"NumberOfDays":${obj.NumberOfDays},"DataPeriod":"${obj.DataPeriod}","Elements":[{"Symbol":"${obj.Symbol}","Type":"price","Params":["c"]}]}`
         return new Promise((resolve: any, reject: any) => {
-            request(url, (error: any, response: any, body: any) => {
-                return (!error && response.statusCode == 200) ? resolve(body) : reject(error)
+            stockHistoryRequest(url, (error: any, response: any, body: any) => {
+                if (!error && response.statusCode == 200) {
+                    stockHistorydb.knex.raw(`delete from stock_history`).then(() => {
+                        let data = JSON.parse(body)
+                        let datesArray = data.Dates.map(date => new Date(date.substring(0, 10)).getTime()/1000)
+                        let pricesArray = data.Elements[0].DataSeries.close.values
+                        let databaseArray: any[] = []
+                        for (let i = 0; i < datesArray.length; i++) {
+                            databaseArray.push(
+                                stockHistorydb.knex.raw(`insert into stock_history values (default, 1, 'netflix', '${data.Elements[0].Symbol}', ${pricesArray[i]}, '${obj.DataPeriod}', ${datesArray[i]})`).then((data) => {
+                                    return data
+                                })
+                            )
+                        }
+                        Promise.all(databaseArray).then((promiseData) => {
+                            return stockHistorydb.knex.raw(`select * from stock_history`).then(finalData => resolve(finalData.rows))
+                        })
+                    })
+                } else {
+                    reject(error)
+                }
             })
         })
     }
