644d1a7929ce653c8394f1ebbbec1ce85077ea68
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-01 23:46:23 -0600

    added route to group history

--- app/routes/API/stockHistory.js
+++ app/routes/API/stockHistory.js
@@ -4,6 +4,11 @@ var router = express.Router();
 var request = require('request');
 var StockHistoryClass = require('../../Models/stocks/stockHistoryModel');
 var stockHistory = new StockHistoryClass;
+router.post('/groupBy', function (req, res, next) {
+    stockHistory.groupStockHistory(req.body.grouping).then(function (data) {
+        res.json(data.rows);
+    });
+});
 router.post('/', function (req, res, next) {
     stockHistory.getStockHistory(req.body).then(function (data) {
         res.json(data);
--- app/routes/API/stockHistory.ts
+++ app/routes/API/stockHistory.ts
@@ -7,11 +7,22 @@ const StockHistoryClass = require('../../Models/stocks/stockHistoryModel')
 const stockHistory = new StockHistoryClass
 
 
+router.post('/groupBy', function(req: Request, res: Response, next: Function) {
+    stockHistory.groupStockHistory(req.body.grouping).then(data => {
+        res.json(data.rows)
+    })
+});
+
+
 router.post('/', function(req: Request, res: Response, next: Function) {
     stockHistory.getStockHistory(req.body).then(data => {
         res.json(data)
     })
 });
+
+
+
+
 
 
 module.exports = router;
